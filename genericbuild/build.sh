730ce09291791c2412f18684168d5a1ff5547ad8
sateffen <sateffen@gmail.com>
2025-08-14 21:12:45 +0200

    Upcated coredns to 1.12.3

--- genericbuild/build.sh
+++ genericbuild/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Configure the version to use here.
-corednsVersion=1.12.2
+corednsVersion=1.12.3
 
 ###############################################################################################
 # Below this is the actual build. Don't change anything, if you don't know what you are doing #
