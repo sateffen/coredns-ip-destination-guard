7a8394e9b7a2cf3febfc46c089611564d7392b68
sateffen <sateffen@gmail.com>
2026-01-12 21:39:22 +0100

    Updated coredns to 1.14.0

--- genericbuild/build.sh
+++ genericbuild/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Configure the version to use here.
-corednsVersion=1.13.1
+corednsVersion=1.14.0
 
 ###############################################################################################
 # Below this is the actual build. Don't change anything, if you don't know what you are doing #
